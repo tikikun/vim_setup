6f293ac273e9f0761105fdb84465000ce67e4c27
tikikun <daogiatuank54@gmail.com>
2023-09-21 15:22:06 +0700

    add hotkey to delete buffer inside telescope

--- init.lua
+++ init.lua
@@ -293,7 +293,7 @@ require('telescope').setup {
     mappings = {
       i = {
         ['<C-u>'] = false,
-        ['<C-d>'] = false,
+        ['<C-d>'] = require('telescope.actions').delete_buffer,
       },
     },
   },
