ab71e5e0dee1f29ed79cf5bbcdb577e84661dd5d
Alan Dao <contact@daogiatuan.com>
2023-09-28 21:50:41 +0700

    open float error

--- lua/custom/plugins/non-mason-lsp-configs.lua
+++ lua/custom/plugins/non-mason-lsp-configs.lua
@@ -23,10 +23,8 @@ return {
 
           vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
         end
-        
         -- My custom config
         nmap('<space>e',vim.diagnostic.open_float,"Open Float Error")
-        
         nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
         nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
         nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
