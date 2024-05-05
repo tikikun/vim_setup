e7609afa215603ce67d3a8bb7bac411ee40b2d2b
Alan Dao <contact@daogiatuan.com>
2024-05-05 14:04:03 +0700

    add custom lua script

--- init.lua
+++ init.lua
@@ -688,4 +688,4 @@ end
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 
-require('image_paste').setup()
+require('custom.utils.image_paste').setup()
